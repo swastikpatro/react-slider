8f7764f3ef51ea8cc286f3abd5778128e86a4920
swastikpatro <patroswastik.jsdev@gmail.com>
2022-12-03 18:23:37 +0530

    initial commit

---
+++ slider-project/src/data.ts
@@ -0,0 +1,82 @@
+const people = [
+  {
+    img: 'https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959121/person-1_aufeoq.jpg',
+    name: 'peter doe',
+    job: 'product manager',
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis? `,
+  },
+  {
+    img: 'https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959131/person-2_ipcjws.jpg',
+    name: 'susan doe',
+    job: 'developer',
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img: 'https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959131/person-3_rxtqvi.jpg',
+    name: 'emma doe',
+    job: 'designer',
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img: 'https://randomuser.me/api/portraits/men/92.jpg',
+    name: 'Chrisitan Pai',
+    job: 'Data Analyst',
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img: 'https://randomuser.me/api/portraits/women/47.jpg',
+    name: 'Serena Robert',
+    job: 'Research Analyst',
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img: 'https://randomuser.me/api/portraits/women/40.jpg',
+    name: 'Maxine Hamilton',
+    job: 'Backend Engineer',
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img: 'https://randomuser.me/api/portraits/men/42.jpg',
+    name: 'Akash Naik',
+    job: 'CEO',
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img: 'https://randomuser.me/api/portraits/women/55.jpg',
+    name: 'Letitia Hernandez',
+    job: 'Fashion Designer',
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img: 'https://randomuser.me/api/portraits/men/10.jpg',
+    name: 'Brennan Wilson',
+    job: 'FrontEnd Engineer',
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img: 'https://randomuser.me/api/portraits/women/10.jpg',
+    name: 'Rita James',
+    job: 'Data Scientist',
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img: 'https://randomuser.me/api/portraits/women/61.jpg',
+    name: 'Jacquelene Thomas',
+    job: 'Sales',
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img: 'https://randomuser.me/api/portraits/women/31.jpg',
+    name: 'Elina Sanders',
+    job: 'Market Analyst',
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img: 'https://randomuser.me/api/portraits/men/79.jpg',
+    name: 'Theodore Butler',
+    job: 'Barber',
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+];
+
+export default people;
